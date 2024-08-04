ff2b797c8179d6cc8695b7a880465f864febe05a
AKASH THAPLIYAL <102138809+akashTh00@users.noreply.github.com>
2024-08-04 12:18:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./Component/Contact";
+import About from "./Component/About";
+import Footer from "./Component/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
